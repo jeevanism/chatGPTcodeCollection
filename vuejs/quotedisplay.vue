57e222ea93e50aafd2f3fc7bf59fac12908dc224
Jeevachaithanyan Sivanandan <jeevanism@gmail.com>
2023-03-29 17:52:11 +0100

    Add files via upload

---
+++ vuejs/quotedisplay.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Random Buddha Quote</h1>
+    <p>{{ quote }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const quote = ref('')
+
+onMounted(async () => {
+  const response = await fetch('https://buddha-api.com/api/random')
+  const data = await response.json()
+  quote.value = data.quote
+})
+</script>
+
+
